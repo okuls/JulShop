c95774274557cefaa6745481f44238e15c85702e
okuls <oskul@yandex.ru>
2018-12-12 18:45:31 +0300

    Product Page

--- julshop/products/urls.py
+++ julshop/products/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from landing import views
-from . import views
+from products import views
 
 urlpatterns = [
+    url(r'^product/(?P<product_id>\w+)/$', views.product, name='product'),
     # url(r'^$', views.ProductList, name='ProductList'),
     # url(r'^(?P<category_slug>[-\w]+)/$', views.ProductList, name='ProductListByCategory'),
     # url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.ProductDetail, name='ProductDetail'),
